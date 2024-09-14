bf0766039348883636d6ad6dbc09a5ff828f4c63
Sanju Naskar <sanjunaskar102@gmail.com>
2024-09-14 23:05:00 +0530

    Task board added

--- task/views.py
+++ task/views.py
@@ -3,13 +3,36 @@ from django.contrib.auth.models import User
 from .models import Task
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
+from django.utils import timezone
 
-# Create your views here.
 @csrf_protect
 def mytask(request):
-    creator = User.objects.get(username=request.user.username)
-    tasks=Task.objects.filter(creator=creator)
-    return render(request,'mytask.html',{'tasks':tasks})
+    if request.user.is_authenticated:
+        creator = User.objects.get(username=request.user.username)
+        tasks = Task.objects.filter(creator=creator)
+
+        # Get the current timezone-aware datetime
+        now = timezone.now()
+
+        for task in tasks:
+            if task.exp_end_date and task.exp_end_date < now:
+                task.task_status = 'Delayed'
+                task.task_status_tag="danger"
+            else:
+                task.task_status = 'On Time'
+                task.task_status_tag="success"
+            
+            # Task priority tag based on the priority level
+            if task.priority == 'High':
+                task.priority_tag = 'danger'  # Bootstrap danger (red)
+            elif task.priority == 'Medium':
+                task.priority_tag = 'warning'  # Bootstrap warning (yellow)
+            else:
+                task.priority_tag = 'primary'  # Bootstrap primary (blue)
+        # Render the tasks with the calculated task_status in the template
+        return render(request, 'mytask.html', {'tasks': tasks})
+
+    return redirect('login')
 
 @csrf_protect
 def task_create(request):
